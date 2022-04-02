472865e9f7bd486b9d44c27d102e7b71fd34f76b
L2ncE <llance_24@foxmail.com>
2022-04-02 15:26:19 +0800

    feat(cmd):初始化主函数

---
+++ cmd/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"CSA/api"
+	"CSA/dao"
+	"fmt"
+)
+
+func main() {
+	err := dao.InitDB()
+	if err != nil {
+		fmt.Printf("init DB failed, err:%v\n", err)
+	} else {
+		fmt.Println("连接MySQL数据库成功!")
+	}
+	err = dao.InitRedis()
+	if err != nil {
+		fmt.Printf("init redis failed, err:%v\n", err)
+	} else {
+		fmt.Println("连接Redis数据库成功!")
+	}
+	err = dao.InitGormDB()
+	if err != nil {
+		fmt.Printf("init redis failed, err:%v\n", err)
+	} else {
+		fmt.Println("连接GORM MySQL数据库成功!")
+	}
+	api.InitEngine()
+}
