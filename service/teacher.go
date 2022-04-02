863c50b11e98a9c697fe7c1341f3369a573b3f04
L2ncE <llance_24@foxmail.com>
2022-04-02 17:09:18 +0800

    feat(service):新增老师修改密码及检验密码正确性相关服务

--- service/teacher.go
+++ service/teacher.go
@@ -10,6 +10,22 @@ func RegisterT(user model.Teacher) error {
 	return err
 }
 
+func ChangeTPassword(Name, newPassword string) error {
+	err := dao.UpdateTPassword(Name, newPassword)
+	return err
+}
+
+//func IsTPasswordCorrect(Name, password string) (bool, error) {
+//	user, err := dao.SelectStuInfoByName(Name)
+//	if err != nil {
+//		return false, err
+//	}
+//	if user.Password != password {
+//		return false, nil
+//	}
+//	return true, nil
+//}
+
 func GetAnswerByTName(Name string) string {
 	answer := dao.SelectAnswerByTName(Name)
 	return answer
