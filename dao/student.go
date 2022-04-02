9885010fb5c8994ee83a6b0adc2499774d8730a2
L2ncE <llance_24@foxmail.com>
2022-04-02 17:05:37 +0800

    feat(dao):新增学生修改密码及检验密码正确性相关数据访问

--- dao/student.go
+++ dao/student.go
@@ -15,6 +15,27 @@ func InsertStu(user model.Student) error {
 	return err
 }
 
+func UpdateStuPassword(Name string, newPassword string) error {
+	deRes := db.Model(&model.Student{}).Where("Name = ?", Name).Update("Password", newPassword)
+	err := deRes.Error
+	if err != nil {
+		fmt.Printf("update failed, err:%v\n", err)
+		return err
+	}
+	return err
+}
+
+func SelectStuInfoByName(Name string) (model.StudentInfo, error) {
+	var user model.StudentInfo
+	dbRes := db.Model(&model.Student{}).Select("id", "password").Where("Name = ?", Name).First(&user)
+	err := dbRes.Error
+	if err != nil {
+		return user, err
+	}
+	fmt.Println(user)
+	return user, nil
+}
+
 func SelectAnswerByStuName(Name string) string {
 	user := model.Student{}
 	db.Model(&model.Student{}).Select("answer").Where("Name = ?", Name).Find(&user)
