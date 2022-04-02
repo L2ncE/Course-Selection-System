6b99ff1638c383c51c3408142b09b18a380012f2
L2ncE <llance_24@foxmail.com>
2022-04-02 17:05:48 +0800

    feat(dao):新增老师修改密码及检验密码正确性相关数据访问

--- dao/teacher.go
+++ dao/teacher.go
@@ -15,6 +15,27 @@ func InsertT(user model.Teacher) error {
 	return err
 }
 
+func UpdateTPassword(Name string, newPassword string) error {
+	deRes := db.Model(&model.Teacher{}).Where("Name = ?", Name).Update("Password", newPassword)
+	err := deRes.Error
+	if err != nil {
+		fmt.Printf("update failed, err:%v\n", err)
+		return err
+	}
+	return err
+}
+
+func SelectTInfoByName(Name string) (model.TeacherInfo, error) {
+	var user model.TeacherInfo
+	dbRes := db.Model(&model.Teacher{}).Select("id", "password").Where("Name = ?", Name).First(&user)
+	err := dbRes.Error
+	if err != nil {
+		return user, err
+	}
+	fmt.Println(user)
+	return user, nil
+}
+
 func SelectAnswerByTName(Name string) string {
 	user := model.Teacher{}
 	db.Model(&model.Teacher{}).Select("answer").Where("Name = ?", Name).Find(&user)
