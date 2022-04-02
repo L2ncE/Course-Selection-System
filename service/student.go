4b4e5618d8114394863fceffc99e73a96e00c64f
L2ncE <llance_24@foxmail.com>
2022-04-02 17:10:37 +0800

    feat(service):新增学生修改密码及检验密码正确性相关服务

--- service/student.go
+++ service/student.go
@@ -10,6 +10,33 @@ func RegisterStu(user model.Student) error {
 	return err
 }
 
+func ChangeStuPassword(Name, newPassword string) error {
+	err := dao.UpdateStuPassword(Name, newPassword)
+	return err
+}
+
+func IsStuPasswordCorrect(Name, password string) (bool, error) {
+	user, err := dao.SelectStuInfoByName(Name)
+	if err != nil {
+		return false, err
+	}
+	if user.Password != password {
+		return false, nil
+	}
+	return true, nil
+}
+
+//func IsRepeatUsername(Name string) (bool, error) {
+//	_, err := dao.SelectStuInfoByName(Name)
+//	if err != nil {
+//		if err == gorm.ErrRecordNotFound {
+//			return false, nil
+//		}
+//		return false, err
+//	}
+//	return true, nil
+//}
+
 func GetAnswerByStuName(Name string) string {
 	answer := dao.SelectAnswerByStuName(Name)
 	return answer
