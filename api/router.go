1e80394b6cf39e3254e471d5faa82c42eda2c4be
L2ncE <llance_24@foxmail.com>
2022-04-06 11:17:49 +0800

    refactor(api):重构路由

--- api/router.go
+++ api/router.go
@@ -9,6 +9,32 @@ func InitEngine() {
 	engine := gin.Default()
 	engine.Use(CORS())
 
+	engine.GET("/major", searchMajor) //得到专业ID NAME 对照表
+
+	studentGroup := engine.Group("/stu") //学生
+	{
+		studentGroup.POST("/register", registerStu)    //注册
+		studentGroup.POST("/login", loginStu)          //登录
+		studentGroup.POST("/ss", stuSecretSecurity)    //密保功能
+		studentGroup.POST("/ss/question", stuQuestion) //查询密保问题
+		{
+			studentGroup.Use(JWTAuth)                        //需要token
+			studentGroup.PUT("/password", changeStuPassword) //修改密码
+		}
+	}
+
+	teacherGroup := engine.Group("/t") //老师
+	{
+		teacherGroup.POST("/register", registerT)    //注册
+		teacherGroup.POST("/login", loginT)          //登录
+		teacherGroup.POST("/ss", tSecretSecurity)    //密保功能
+		teacherGroup.POST("/ss/question", tQuestion) //查询密保问题
+		{
+			teacherGroup.Use(JWTAuth)                      //需要token
+			teacherGroup.PUT("/password", changeTPassword) //修改密码
+		}
+	}
+
 	err := engine.Run(":8081")
 	if err != nil {
 		fmt.Printf("init error:%v\n", err)
