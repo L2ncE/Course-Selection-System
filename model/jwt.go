871ab6c69ea58978d5111dfbdd1ff84c6d051bbf
L2ncE <llance_24@foxmail.com>
2022-04-07 15:30:16 +0800

    refactor(model):更改jwt模型

--- model/jwt.go
+++ model/jwt.go
@@ -3,7 +3,9 @@ package model
 import "github.com/dgrijalva/jwt-go"
 
 type MyClaims struct {
+	ID       string `json:"ID"`
 	Username string `json:"username"`
 	Password string `json:"password"`
+	Identity string `json:"identity"`
 	jwt.StandardClaims
 }
