4c20552c2f1f6bcd9b0220139508f45046669017
L2ncE <llance_24@foxmail.com>
2022-04-07 17:35:15 +0800

    refactor(model):重构jwt模型

--- model/jwt.go
+++ model/jwt.go
@@ -3,7 +3,7 @@ package model
 import "github.com/dgrijalva/jwt-go"
 
 type MyClaims struct {
-	ID       string `json:"ID"`
+	ID       int    `json:"ID"`
 	Username string `json:"username"`
 	Password string `json:"password"`
 	Identity string `json:"identity"`
