e6efec034980617881d46355deb59eaec8515513
L2ncE <llance_24@foxmail.com>
2022-04-11 15:21:27 +0800

    feat(service):将课程相关的数据访问对象封装为服务

--- service/course.go
+++ service/course.go
@@ -1,1 +1,41 @@
 package service
+
+import (
+	"CSA/dao"
+	"CSA/model"
+)
+
+func RegisterCourse(c model.Course) error {
+	err := dao.InsertCourse(c)
+	return err
+}
+
+func RemoveCourse(id int) error {
+	err := dao.DeleteCourse(id)
+	return err
+}
+
+func ChangeCourseTime(id int, time string) error {
+	err := dao.UpdateCourseTime(id, time)
+	return err
+}
+
+func ChangeCourseCredit(id int, credit string) error {
+	err := dao.UpdateCourseCredit(id, credit)
+	return err
+}
+
+func ChangeCourseName(id int, name string) error {
+	err := dao.UpdateCourseName(id, name)
+	return err
+}
+
+func ChangeCourseTotal(id int, total int) error {
+	err := dao.UpdateCourseTotal(id, total)
+	return err
+}
+
+func GetAllCourse() error {
+	err := dao.SelectCourse()
+	return err
+}
