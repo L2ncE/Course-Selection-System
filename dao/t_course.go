ff3a1f46604e2158ba1fb1dd88fa0d63587db61d
L2ncE <llance_24@foxmail.com>
2022-04-14 17:08:57 +0800

    feat(dao):更新老师课程数据访问

--- dao/t_course.go
+++ dao/t_course.go
@@ -14,3 +14,41 @@ func InsertTCourse(course model.TCourse) error {
 	}
 	return err
 }
+
+func DeleteTCourse(id int) error {
+	var Course []model.TCourse
+	dbRes := db.Where("Id = ?", id).Delete(&Course)
+	err := dbRes.Error
+	if err != nil {
+		fmt.Printf("delete failed, err:%v\n", err)
+		return err
+	}
+	return err
+}
+
+func UpdateTCourseTime(id int, time string) error {
+	deRes := db.Model(&model.TCourse{}).Where("id = ?", id).Update("Time", time)
+	err := deRes.Error
+	if err != nil {
+		fmt.Printf("update failed, err:%v\n", err)
+		return err
+	}
+	return err
+}
+
+func UpdateCourseTotal(id int, total int) error {
+	deRes := db.Model(&model.TCourse{}).Where("id = ?", id).Update("Total", total)
+	err := deRes.Error
+	if err != nil {
+		fmt.Printf("update failed, err:%v\n", err)
+		return err
+	}
+	return err
+}
+
+func SelectTCourse() ([]model.TCourse, error) {
+	var Course []model.TCourse
+	dbRes := db.Model(&model.Course{}).Find(&Course)
+	err := dbRes.Error
+	return Course, err
+}
