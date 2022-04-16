4a64c10a71de223770f5c9c8c94a8dee2a77c36b
L2ncE <llance_24@foxmail.com>
2022-04-16 11:29:34 +0800

    feat(model):增加学生相关结构

--- model/student.go
+++ model/student.go
@@ -17,3 +17,10 @@ type StudentInfo struct {
 	NickName string `gorm:"column:NickName"`
 	Password string
 }
+
+type StudentInfo2 struct {
+	Id       int
+	Name     string
+	MajorNum int    `gorm:"column:MajorNum"`
+	NickName string `gorm:"column:NickName"`
+}
