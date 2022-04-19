9fb2aa52a30fe0224f789f4ef4d1703cd947cef9
L2ncE <llance_24@foxmail.com>
2022-04-19 15:22:10 +0800

    refactor(api):重构中间件，删除冗余注释

--- api/middleware.go
+++ api/middleware.go
@@ -48,20 +48,3 @@ func CORS() gin.HandlerFunc {
 		ctx.Next()
 	}
 }
-
-//func TlsHandler(port int) gin.HandlerFunc {
-//	return func(c *gin.Context) {
-//		secureMiddleware := secure.New(secure.Options{
-//			SSLRedirect: true,
-//			SSLHost:     ":" + strconv.Itoa(port),
-//		})
-//		err := secureMiddleware.Process(c.Writer, c.Request)
-//
-//		// If there was an error, do not continue.
-//		if err != nil {
-//			return
-//		}
-//
-//		c.Next()
-//	}
-//}
