a25d4c90a9b41614c692c72ff9aebf7151844dea
L2ncE <llance_24@foxmail.com>
2022-04-20 10:07:57 +0800

    refactor(cmd):重构主函数数据连接

--- cmd/main.go
+++ cmd/main.go
@@ -7,19 +7,7 @@ import (
 )
 
 func main() {
-	err := dao.InitDB()
-	if err != nil {
-		fmt.Printf("init DB failed, err:%v\n", err)
-	} else {
-		fmt.Println("连接MySQL数据库成功!")
-	}
-	err = dao.InitRedis()
-	if err != nil {
-		fmt.Printf("init redis failed, err:%v\n", err)
-	} else {
-		fmt.Println("连接Redis数据库成功!")
-	}
-	err = dao.InitGormDB()
+	err := dao.InitGormDB()
 	if err != nil {
 		fmt.Printf("init redis failed, err:%v\n", err)
 	} else {
